546a3d7cae84829453dbdb30e867e37ed6ee426f
SahlaThasnim89 <sahlathasnim2002@gmail.com>
2025-01-18 20:45:12 +0530

    dashboard change

--- controller/userController.js
+++ controller/userController.js
@@ -133,6 +133,9 @@ const dashboard = async (req, res) => {
     const page = Number(req.query.page) || 1;
     const skip = (page - 1) * limit;
     const qrCodeData = await prisma.QRCode.findMany({
+      where: {
+        userId: req.session.userData.id, 
+      },
       skip,
       take: limit,
       orderBy: {
